54a726c0d76287d08180bc46a1d838e97797ce00
Kerlyson <kerlyson_sk@yahoo.com.br>
2020-10-31 11:52:20 -0300

    correcoes JGRAPH

--- src/main/java/util/HelloJGraphT.java
+++ src/main/java/util/HelloJGraphT.java
@@ -4,7 +4,7 @@ import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
-import org.graphstream.ui.swingViewer.Viewer;
+import org.graphstream.ui.view.Viewer;
 
 /**
  * Teste de visualização da AST
@@ -16,27 +16,26 @@ import org.graphstream.ui.swingViewer.Viewer;
 public final class HelloJGraphT {
   public static void main(String[] args) {
 
-    System.setProperty("org.graphstream.ui.renderer",
-        "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+//    System.setProperty("org.graphstream.ui.renderer",
+//        "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+    System.setProperty("org.graphstream.ui", "swing");
 
     Graph graph = new MultiGraph("Test");
 
-
+    
     Node A = graph.addNode("A");
     Node B = graph.addNode("B");
     Node C = graph.addNode("C");
     Node D = graph.addNode("D");
     Node E = graph.addNode("E");
 
-    A.setAttribute("xyz", 0, 0, 0);
-    B.setAttribute("xyz", -1, -1, 0);
-    C.setAttribute("xyz", 1, -1, 0);
-    D.setAttribute("xyz", -2, -2, 0);
-    E.setAttribute("xyz", 0, -2, 0);
+    A.setAttribute("xy", 0, 0);
+    B.setAttribute("xy", -1, -1);
+    C.setAttribute("xy", 1, -1);
+    D.setAttribute("xy", -2, -2);
+    E.setAttribute("xy", 0, -2);
 
-    for (Node n : graph.getNodeSet()) {
-      n.addAttribute("ui.label", n.getId());
-    }
+    graph.nodes().forEach(node -> node.setAttribute("ui.label", node.getId()));
 
     Edge a = graph.addEdge("1", "A", "B");
     Edge b = graph.addEdge("2", "A", "B");
@@ -44,8 +43,10 @@ public final class HelloJGraphT {
     Edge d = graph.addEdge("4", "B", "D");
     Edge e = graph.addEdge("5", "B", "E");
 
+    graph.setAttribute("ui.stylesheet","node { shape: box; fill-color: #DEE; size: 100px, 50px; stroke-mode: plain; stroke-color: #555;}");
+    graph.setAttribute("ui.antialias");
     Viewer viewer = graph.display(false);
-
+    
 
   }
 }
