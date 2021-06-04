f3d22a8dc7d5ef876686964d55ca8e59ca22d7e1
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2021-06-04 17:32:45 -0300

    formatacao

--- src/main/java/debug/DebugSnapshot.java
+++ src/main/java/debug/DebugSnapshot.java
@@ -10,29 +10,29 @@ import modelos.tree.AstDebugNode;
  *
  */
 public class DebugSnapshot {
-    private AstDebugNode node;
-    private Map<String, Object> ambienteSnapshot;
+	private AstDebugNode node;
+	private Map<String, Object> ambienteSnapshot;
 
-    public DebugSnapshot(AstDebugNode node, Map<String, Object> ambienteSnapshot) {
-	super();
-	this.node = node;
-	this.ambienteSnapshot = ambienteSnapshot;
-    }
+	public DebugSnapshot(AstDebugNode node, Map<String, Object> ambienteSnapshot) {
+		super();
+		this.node = node;
+		this.ambienteSnapshot = ambienteSnapshot;
+	}
 
-    public AstDebugNode getNode() {
-	return node;
-    }
+	public AstDebugNode getNode() {
+		return node;
+	}
 
-    public Map<String, Object> getAmbienteSnapshot() {
-	return ambienteSnapshot;
-    }
+	public Map<String, Object> getAmbienteSnapshot() {
+		return ambienteSnapshot;
+	}
 
-    public int getLinha() {
-	return node.getLinha();
-    }
+	public int getLinha() {
+		return node.getLinha();
+	}
 
-    public String getNodeClassName() {
-	return node.getClass().getName();
-    }
+	public String getNodeClassName() {
+		return node.getClass().getName();
+	}
 
 }
