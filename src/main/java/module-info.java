481d592b937e35e53801351f0e7f7ff6436c57d9
Kerlyson Santos <kerlyson_sk@yahoo.com.br>
2021-09-05 20:29:02 -0300

    updates para module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module interpretadorMACP {
+	exports conversores;
+	exports evento;
+	exports debug;
+	exports modelos.tree;
+	exports analisador;
+	exports log;
+	exports interpretador;
+	exports modelos.excecao;
+	exports modelos;
+	exports tool;
+
+	requires JColor;
+	requires cloning;
+	requires gs.core;
+	requires java.desktop;
+	requires pherd;
+	requires scala.library;
+}
